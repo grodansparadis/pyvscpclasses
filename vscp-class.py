5ff2d91b34088b8e4ac991462d27b3aed29183c1
Ake Hedman <akhe@grodansparadis.com>
2024-11-05 21:37:54 +0100

    Fixed VSCP2_TYPE_PROTOCOL_WRITE_REGISTER_DIRECT

--- vscp-class.py
+++ vscp-class.py
@@ -19,7 +19,7 @@
 #            !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
 #                           This file is auto-generated
 #                see https://github.com/grodansparadis/vscp-classes
-#                        Generated: 2024-11-01 12:00:43.133873
+#                        Generated: 2024-11-05 21:03:53.049764
 #
  
 VSCP_CLASS1_PROTOCOL                     = 0      # VSCP Protocol Functionality
